c1e372bffa7032dbe8103fe73ba088a470166cf5
GamesGosling427 <111503422+GamesGosling427@users.noreply.github.com>
2022-08-19 03:15:18 +0530

    Add files via upload

---
+++ Employ_1_driver.java
@@ -0,0 +1,16 @@
+package encapsulation;
+
+public class Employ_1_driver {
+
+	public static void main(String[] args) {
+		Employ_1 e1=new Employ_1();
+			e1.setName("rohan");
+			e1.setSalary("12345678");
+			System.out.println(e1.getName());
+			System.out.println(e1.getSalary());
+			
+		}
+
+	}
+
+
